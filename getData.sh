ddba0fb4f747cc1ee3657c9004d743259958951c
qing-duan <qing.duan@cchmc.org>
2022-05-24 12:58:50 -0400

    Adding files to github

---
+++ getData.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+    set -e
+    set -u
+    set -o pipefail
+
+addr_infile=$1
+addr_outfile=$2
+
+echo "script name: $0"
+echo "address raw file: ${addr_infile}"
+echo "address clean file: ${addr_outfile}"
+
+# organize address
+./scripts/get_address.R ${addr_infile} ${addr_outfile}
+
+# geocoder
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/geocoder:3.2.0 ./data/${addr_outfile}.csv
+
+# dep-index
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/dep_index:0.2.0 ./data/${addr_outfile}_geocoder_3.2.0_score_threshold_0.5.csv
+
+# roads
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/roads:0.2.1 ./data/${addr_outfile}_geocoder_3.2.0_score_threshold_0.5_dep_index_0.2.0.csv
+
+# aadt
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/aadt:0.2.0 ./data/${addr_outfile}_geocoder_3.2.0_score_threshold_0.5_dep_index_0.2.0_roads_0.2.1_400m_buffer.csv
+
+# greenspace
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/greenspace:0.3.0 ./data/${addr_outfile}_geocoder_3.2.0_score_threshold_0.5_dep_index_0.2.0_roads_0.2.1_400m_buffer_aadt_0.2.0_400m_buffer.csv
+
+# drivetime
+docker run --rm -v $PWD:/tmp ghcr.io/degauss-org/drivetime:1.1.0 ./data/${addr_outfile}_geocoder_3.2.0_score_threshold_0.5_dep_index_0.2.0_roads_0.2.1_400m_buffer_aadt_0.2.0_400m_buffer_greenspace_0.3.0.csv cchmc
+
+
+
---
+++ get_address.R
@@ -0,0 +1,24 @@
+#!/usr/bin/env Rscript
+
+args=commandArgs(trailingOnly = TRUE)
+
+input = args[1]
+output = args[2]
+
+library(tidyverse)
+
+addrfile <- read_csv(paste0("raw-data/", input, ".csv"))
+
+addrfile <- addrfile %>% 
+  mutate(pat_zip_clean = str_split(pat_zip, "-", simplify=TRUE)[,1]) %>% 
+  unite("address", c(pat_addr_1, pat_city, pat_state, pat_zip_clean), remove = FALSE, sep=" ")
+
+outfile = paste0("data/", output, ".csv")
+write_csv(addrfile, outfile)
+
+
+
+
+
+
+
