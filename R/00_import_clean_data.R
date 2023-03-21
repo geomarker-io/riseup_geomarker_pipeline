c758ed52e15c1db65568a68875b3b5c79dd1cd99
Cole Brokamp <cole.brokamp@gmail.com>
2023-03-21 18:00:30 -0400

    only read in columns required for linkage

--- R/00_import_clean_data.R
+++ R/00_import_clean_data.R
@@ -1,21 +1,26 @@
-library(tidyverse)
-library(lubridate)
-library(dht)
+## library(tidyverse)
+library(dplyr)
+library(readr)
+## library(lubridate)
+## library(dht)
 
 # read in raw admission data
-d <- readr::read_csv("data-raw/Hospital Admissions.csv", 
-                     na = c("NA", "-", "NULL")) # n = 135871 records
-dim(d)
+d <- read_csv("data-raw/Hospital Admissions.csv",
+              na = c("NA", "-", "NULL"),
+              col_types = cols_only(PAT_MRN_ID = col_character(),
+                                    PAT_ENC_CSN_ID = col_character(),
+                                    HOSP_ADMSN_TIME = col_date(),
+                                    PAT_ADDR_1 = col_character(),
+                                    PAT_ADDR_2 = col_character(),
+                                    PAT_CITY = col_character(),
+                                    PAT_STATE = col_character(),
+                                    PAT_ZIP = col_character()))
 
 # remove a duplicated record (based on PAT_ENC_CSN_ID) from current dataset
-d <- d |> 
-  filter(!(PAT_ENC_CSN_ID == "549864470" & MAPPED_RACE == "Unknown")) # 135870
-dim(d)
+d <- d |> filter(!(PAT_ENC_CSN_ID == "549864470" & MAPPED_RACE == "Unknown"))
 
 # limit data to patients admitted between 1/1/2016 and 12/31/2021
-d <- d |> 
-  filter(HOSP_ADMSN_TIME <= ymd(20211231)) # 124244
-dim(d)
+d <- d |> filter(HOSP_ADMSN_TIME <= ymd(20211231))
 
 d.address <- d |>  
   separate(PAT_ZIP, 
