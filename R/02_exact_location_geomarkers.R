0d8cbb86c27fabe77c21634b92e9b382da7a6d22
erikarasnick <erika.rasnick@gmail.com>
2023-03-28 08:37:05 -0400

    clean up exact location metadata

--- R/02_exact_location_geomarkers.R
+++ R/02_exact_location_geomarkers.R
@@ -24,19 +24,19 @@ d <- d |>
 # add column attributes
 d <- d |>
   add_col_attrs(parsed_address,
-                title = 'parsed address',
+                title = 'Parsed Address',
                 description = 'parsed address'
                 ) |>
   add_col_attrs(lat,
-                title = 'lat',
+                title = 'Latitude',
                 description = 'geocoded latitude coordinate'
                 ) |>
   add_col_attrs(lon,
-                title = 'lon',
+                title = 'Longitude',
                 description = 'geocoded longitude coordinate'
                 ) |>
   add_col_attrs(geocode_result,
-                title = 'geocode result',
+                title = 'Geocode Result',
                 description = 'character string summarizing the geocoding result (geocoded: the address was geocoded with a precision of either range or street and a score of 0.5 or greater; imprecise_geocode: the address was geocoded, but results were suppressed because the precision was intersection, zip, or city and/or the score was less than 0.5; po_box: the address was not geocoded because it is a PO Box; cincy_inst_foster_addr: the address was not geocoded because it is a known institutional address, not a residential address; non_address_text: the address was not geocoded because it was blank or listed as “foreign”, “verify”, or “unknown”)'
                 ) |>
   add_col_attrs(census_block_group_id_2010,
@@ -52,64 +52,65 @@ d <- d |>
                 title = '2020 Census Tract ID'
   ) |>
   add_col_attrs(dist_to_1100,
-                title = 'dist to 1100',
+                title = 'Distance to Nearest Primary Road',
                 description = 'distance (meters) to the nearest S1100 road'
                 ) |>
   add_col_attrs(dist_to_1200,
-                title = 'dist to 1200',
+                title = 'Distance to Nearest Secondary Road',
                 description = 'distance (meters) to the nearest S1200 road'
                 ) |>
   add_col_attrs(length_1100,
-                title = 'length 1100',
+                title = 'Length of Primary Roads',
                 description = 'length (meters) of S1100 roads within a 400 m buffer'
                 ) |>
   add_col_attrs(length_1200,
-                title = 'length 1200',
+                title = 'Length of Secondary Roads',
                 description = 'length (meters) of S1200 roads within a 400 m buffer'
                 ) |>
   add_col_attrs(length_stop_go,
-                title = 'length stop go',
-                description = 'total length of arterial roads (meters)'
+                title = 'Length of Roads with Stop and Go Traffic',
+                description = 'total length of arterial roads (meters) within 400 m'
                 ) |>
   add_col_attrs(length_moving,
-                title = 'length moving',
-                description = 'total length of interstates, expressways, and freeways (meters)'
+                title = 'Length of Roads with Moving Traffic',
+                description = 'total length of interstates, expressways, and freeways (meters) within 400 m'
                 ) |>
   add_col_attrs(vehicle_meters_stop_go,
-                title = 'vehicle meters stop go',
+                title = 'Stop and Go Traffic (vehicle-m)',
                 description = 'average daily number of vehicles multiplied by the length of arterial roads (vehicle-meters)'
                 ) |>
   add_col_attrs(vehicle_meters_moving,
-                title = 'vehicle meters moving',
+                title = 'Moving Traffic (vehicle-m)',
                 description = 'average daily number of vehicles multiplied by the length of interstates, expressways, and freeways (vehicle-meters)'
                 ) |>
   add_col_attrs(truck_meters_stop_go,
-                title = 'truck meters stop go',
+                title = 'Stop and Go Truck Traffic (truck-m)',
                 description = 'average daily number of trucks multiplied by the length of arterial roads (truck-meters)'
                 ) |>
   add_col_attrs(truck_meters_moving,
-                title = 'truck meters moving',
+                title = 'Moving Truck Traffic (truck-m)',
                 description = 'average daily number of trucks multiplied by the length of interstates, expressways, and freeways (truck-meters)'
                 ) |>
   add_col_attrs(evi_500,
-                title = 'evi 500',
+                title = 'EVI (500m buffer)',
                 description = 'average enhanced vegetation index within a 500 meter buffer radius'
                 ) |>
   add_col_attrs(evi_1500,
-                title = 'evi 1500',
+                title = 'EVI (1500m buffer)',
                 description = 'average enhanced vegetation index within a 1500 meter buffer radius'
                 ) |>
   add_col_attrs(evi_2500,
-                title = 'evi 2500',
+                title = 'EVI (2500m buffer)',
                 description = 'average enhanced vegetation index within a 2500 meter buffer radius'
                 ) |>
   add_col_attrs(drive_time,
-                title = 'drive time',
+                title = 'Drive Time to CCHMC',
                 description = 'drive time in minutes (in 6 minute intervals, ">60" if more than 1 hour drive time)'
                 ) |>
   add_col_attrs(distance,
-                title = 'distance',
+                title = 'Distance to CCHMC',
                 description = 'distance in meters'
-                )
+                ) |>
+  add_type_attrs()
 
 saveRDS(d, "data/exact_location_geomarkers.rds")
