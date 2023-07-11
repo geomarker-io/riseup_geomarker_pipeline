77cfaeee545cbfd8e6dbe3e7683f2944badea003
Cole Brokamp <cole.brokamp@gmail.com>
2023-07-10 23:36:17 -0400

    don't include pct_green from landcover

--- R/add_nlcd.R
+++ R/add_nlcd.R
@@ -31,59 +31,6 @@ d.buffered <- d.point |>
 
 d.buffered <- terra::vect(d.buffered)
 
-# nlcd_legend
-nlcd_legend <-
-  tibble::tribble(
-    ~value, ~landcover_class, ~landcover, ~green,
-    11, "water", "water", FALSE,
-    12, "water", "ice/snow", FALSE,
-    21, "developed", "developed open", TRUE,
-    22, "developed", "developed low intensity", TRUE,
-    23, "developed", "developed medium intensity", FALSE,
-    24, "developed", "developed high intensity", FALSE,
-    31, "barren", "rock/sand/clay", FALSE,
-    41, "forest", "deciduous forest", TRUE,
-    42, "forest", "evergreen forest", TRUE,
-    43, "forest", "mixed forest", TRUE,
-    51, "shrubland", "dwarf scrub", TRUE,
-    52, "shrubland", "shrub/scrub", TRUE,
-    71, "herbaceous", "grassland", TRUE,
-    72, "herbaceous", "sedge", TRUE,
-    73, "herbaceous", "lichens", TRUE,
-    74, "herbaceous", "moss", TRUE,
-    81, "cultivated", "pasture/hay", TRUE,
-    82, "cultivated", "cultivated crops", TRUE,
-    90, "wetlands", "woody wetlands", TRUE,
-    95, "wetlands", "emergent herbaceous wetlands", TRUE
-  )
-
-# extract info
-# nlcd
-if (file.exists("rasters/nlcd_hv_2019.tif")) {
-  nlcd_hv_2019 <- terra::rast("rasters/nlcd_hv_2019.tif")
-} else {
-  hv <- cincy::county_hlthv_2010 |>
-    st_union()
-
-  nlcd_tif <- s3::s3_get("s3://geomarker/nlcd_cog/nlcd_landcover_2019.tif")
-  nlcd <- terra::rast(nlcd_tif)
-  nlcd_hv_2019 <- terra::crop(nlcd, hv)
-  terra::writeRaster(nlcd_hv_2019, "rasters/nlcd_hv_2019.tif")
-  fs::dir_delete("s3_downloads")
-}
-
-pct_green <-
-  terra::extract(nlcd_hv_2019, d.buffered) |>
-  rename(value = Layer_1) |>
-  left_join(nlcd_legend, by = "value") |>
-  group_by(ID) |>
-  summarize(pct_green_2019 = round(sum(green) / n() * 100)) |>
-  select(-ID)
-
-d.pct_green <- bind_cols(d.point, pct_green) |>
-  unnest(PAT_ENC_CSN_ID) |>
-  st_drop_geometry()
-
 # impervious
 if (file.exists("rasters/impervious_hv_2019.tif")) {
   impervious_hv_2019 <- terra::rast("rasters/impervious_hv_2019.tif")
@@ -137,16 +84,11 @@ d.pct_treecanopy <- bind_cols(d.point, pct_treecanopy) |>
   st_drop_geometry()
 
 # merge
-d.nlcd <- d.pct_green |>
-  left_join(d.pct_impervious, by = "PAT_ENC_CSN_ID") |>
+d.nlcd <- d.pct_impervious |>
   left_join(d.pct_treecanopy, by = "PAT_ENC_CSN_ID")
 
 # add column attributes
 d <- d |>
-  add_col_attrs(pct_green_2019,
-    title = "percentage green",
-    description = "percent of green = TRUE nlcd cells overlapping buffer (green = TRUE if landcover classification in any category except water, ice/snow, developed medium intensity, developed high intensity, rock/sand/clay)"
-  ) |>
   add_col_attrs(pct_impervious_2019,
     title = "percentage impervious",
     description = "average percent impervious of all nlcd cells overlapping the buffer"
