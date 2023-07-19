6d6f49e5ea99019936a47c26681ee8ae2fdc7890
Cole Brokamp <cole.brokamp@gmail.com>
2023-07-19 15:42:48 -0400

    updated for newer version of parcel package (v0.6.1)

--- R/05_parcel_data.R
+++ R/05_parcel_data.R
@@ -1,35 +1,7 @@
-library(parcel)
+reticulate::use_virtualenv("~/.virtualenvs/r-parcel/") # make sure to use this one if there is a problem with loading the learned settings file
+library(parcel) # must be >= version 0.6.1 
 library(dplyr)
 
-d <- readRDS("data/geocodes.rds")
-
-parcel_links <-
-  d$parsed_address |>
-  unique() |>
-  link_parcel() |>
-  filter(
-    !is.na(parcel_id),
-    land_use != "residential vacant land"
-  )
-left_join(
-  codec::read_tdr_csv(fs::path_package("parcel", "cagis_parcels")),
-  join_by(parcel_id)
-) |>
-  nest_by(input_address, .key = "parcel_data")
-
-d_parcel <-
-  left_join(d,
-    parcel_links,
-    by = join_by(parsed_address == input_address),
-    relationship = "many-to-many"
-  )
-
-# TODO filter to one parcel per address
-# for now, randomly sample one if more than one
-# future code should consider logic for selecting *which* is best one to choose
-d_out <-
-  tidyr::unnest(d_parcel, parcel_data, keep_empty = TRUE) |>
-  slice_sample(n = 1, by = PAT_ENC_CSN_ID)
-
-# save
+d <- readRDS("data/cleaned_addresses.rds")
+d_out <- d |> bind_cols(get_parcel_data(d$address))
 saveRDS(d_out, "data/parcel.rds")
