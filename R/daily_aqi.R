6c6f1f783b999cc377349ecadd4ce95165115789
Cole Brokamp <cole.brokamp@gmail.com>
2023-07-26 17:12:30 -0400

    read aqi from zip files

--- R/daily_aqi.R
+++ R/daily_aqi.R
@@ -1,36 +1,30 @@
 library(dplyr)
 library(codec)
 
-func_download <- function(url, dest) {
-  download.file(url, destfile = dest, mode = "wb")
-  
-  unzip(dest, exdir = "tmp")
-  unlink(dest)
+get_daily_county_aqi <- function(year) {
+  dest_file <- glue::glue("daily_county_aqi_{year}.zip")
+  download.file(glue::glue("https://aqs.epa.gov/aqsweb/airdata/daily_aqi_by_county_{year}.zip"),
+                destfile = dest_file,
+                quiet = TRUE)
+  d <-
+    readr::read_csv(dest_file, show_col_types = FALSE) |>
+    filter(`State Name` == "Ohio" & `county Name` == "Hamilton") |>
+    transmute(
+      date = as.Date(Date),
+      aqi = AQI
+    )
+  on.exit(unlink(dest_file))
+  return(d)
 }
 
-year <- 2015:2022
-dwnld_url <- glue::glue("https://aqs.epa.gov/aqsweb/airdata/daily_aqi_by_county_{year}.zip")
-dest <- glue::glue("tmp/{year}.zip")
-
-message("downloading AQI data...")
-purrr::walk2(dwnld_url, dest, ~ func_download(.x, .y))
-
-file <- glue::glue("tmp/daily_aqi_by_county_{year}.csv")
-
-aqi <- purrr::map_dfr(file, ~ read_csv(.x)) |>
-  filter(`State Name` == "Ohio" & `county Name` == "Hamilton") |>
-  select(
-    date = Date,
-    aqi = AQI
-  ) |>
+out <- purrr::map_dfr(2015:2022, get_daily_county_aqi, .progress = "getting daily county aqi")
+  
+out <- out |>
   add_col_attrs(date,
                 title = "Date",
-                description = "Date"
-  ) |>
+                description = "Date") |>
   add_col_attrs(aqi,
                 title = "AQI",
-                description = "Air Quality Index"
-  )
+                description = "Air Quality Index for Hamilton County, Ohio")
 
-saveRDS(aqi, "data-raw/daily_aqi.rds")
-fs::dir_delete("tmp")
+saveRDS(out, "data-raw/daily_aqi.rds")
